6a7f2522049bce16587e47b1b1902dbd5034fe7d
Natanael Vieira <natanaelsales@live.com>
2022-08-31 06:30:06 -0300

    Create constantes_2.dart

---
+++ fundamentos/constantes_2.dart
@@ -0,0 +1,8 @@
+main() {
+  const lista = ['Thais', 'Natanael', 'Abigail', 'Timóteo', 'Rute'];
+  // se por acaso eu colocar no lugar do 'final' o 'const' da mesma forma eu não consigo alterar nada na lista, nem atribuir uma nova lista ao identificador ou adicionar/remover um dado dentro da lista
+  // lista.add('Joaquim'); // Não se pode colocar mais nenhum nome na lista porque ela é uma constante agora, desse modo não pode ser alterada, nem retirar ou adicionar nomes, neste exemplo.
+  // lista = ['banana', 'maçã']; // Não tem como atribuir uma nova lista ao identificado 'lista' porque ela agora é um final, ou seja, ela agora é uma constante e não tem como mais adicionar esse novo atributo, caso não houvesse o const na lista após a atribuição eu ainda conseguiria adicionar/remover nomes dentro da lista
+
+  print(lista);
+}
