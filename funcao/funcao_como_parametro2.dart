62190976b908a670f10466bd92579bc11c33799f
Natanael Vieira <natanaelsales@live.com>
2022-09-07 07:01:31 -0300

    Update funcao_como_parametro2.dart

--- funcao/funcao_como_parametro2.dart
+++ funcao/funcao_como_parametro2.dart
@@ -1,10 +1,17 @@
-void executarPor(int qtde, Function(String) fn, String valor) {
+int executarPor(int qtde, String Function(String) fn, String valor) {
+  String textoCompleto = '';
   for (int i = 0; i < qtde; i++) {
-    fn(valor);
+    textoCompleto += fn(valor);
   }
+  return textoCompleto.length;
 }
 
 main() {
   print("Testando: 1, 2, 3!");
-  executarPor(10, print, 'É isso aí, está aprendendo Dart!');
+  var meuPrint = (String valor) {
+    print(valor);
+    return valor;
+  };
+  int tamanho = executarPor(10, meuPrint, 'É isso aí, está aprendendo Dart!');
+  print('O tamanho da String é: $tamanho');
 }
