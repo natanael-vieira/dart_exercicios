fabc6b16fe4c2073046500ffe9d93561a3627fc3
Natanael Vieira <natanaelsales@live.com>
2022-09-07 06:05:44 -0300

    Update funcao_como_parametro1.dart
    Inclusão de um comentário.

--- funcao/funcao_como_parametro1.dart
+++ funcao/funcao_como_parametro1.dart
@@ -5,7 +5,9 @@ void executar({required Function fnPar, required Function fnImpar}) {
   print('O valor sorteado foi: $sorteado');
   sorteado % 2 == 0 ? fnPar() : fnImpar();
 
-  /*A função: Random().nextInt(10) % 2 == 0 ? fnPar() : fnImpar();
+  /* OBS.: Inicialmente a linha que havia a Function não tinha o 'required', porém o Dart não permitiu executar uma função 'non null', desta forma tive que adicionar o 'required' antes da Function para ela ser executada normalmente.
+  
+  A função: Random().nextInt(10) % 2 == 0 ? fnPar() : fnImpar();
    também pode ser escrita da seguinte maneira:
   
   if(Random().nextInt(10) % 2 == 0){
