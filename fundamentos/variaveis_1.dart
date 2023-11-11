20e46b31953184abc0e4b678f213010eb4cb188e
Natanael Vieira <natanaelsales@live.com>
2023-11-11 06:18:27 -0300

    Create variaveis_1.dart

---
+++ fundamentos/variaveis_1.dart
@@ -0,0 +1,14 @@
+main() {
+  print(1 + 2 * 4);
+  // Aqui estamos usando valores literais, ou seja diretamente o que preciso que faça sem o uso de variáveis
+
+  int a = 5;
+  print(a);
+
+  double b = 3.1314;
+
+  b = 3.1415;
+  //posso atribuir posteriormente um valor a uma variável que já havia sido atribuída anteriormente
+  print(b);
+  print(a * b);
+}
