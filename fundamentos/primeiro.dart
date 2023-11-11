bf72c98e4d3f25b71554de8298ba1e8f9667777f
Natanael Vieira <natanaelsales@live.com>
2023-11-11 06:08:50 -0300

    Create primeiro.dart

---
+++ fundamentos/primeiro.dart
@@ -0,0 +1,15 @@
+main() {
+  print('Olá Dart!');
+  print("Até o próximo exercício!");
+
+  //Não importa se usamos aspas simples ou duplas, vai depender do gosto, mas se começar com aspas simples, deve-se fechar com aspas simples.
+
+  {
+    ;
+    ;
+    ;
+    // Posso criar blocos e sentenças, dentro do código, vazias, mas não está errado, ele apenas não exibirá nada porque estão vazias.
+  }
+
+  print('Fim!');
+}
