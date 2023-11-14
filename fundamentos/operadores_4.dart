95683c7ce4be9f7243835da970a7dedd68982160
Natanael Vieira <natanaelsales@live.com>
2023-11-14 07:01:03 -0300

    Update operadores_4.dart

--- fundamentos/operadores_4.dart
+++ fundamentos/operadores_4.dart
@@ -1,25 +1,13 @@
+import 'dart:io';
+
 main() {
-  int a = 3;
-  int b = 4;
+  stdout.write('Está chovendo? (s/N) ');
+  bool estaChovendo = stdin.readLineSync() == 's';
 
-  //a = a + 1;
-  //a += 1;
-  a++; // Todas estas opções adicionam mais 1 a variável = Operador unário Postfix
-  --a; // Prefix
+  stdout.write('Está frio? (s/N) ');
+  bool estaFrio = stdin.readLineSync() == 's';
 
-  print(a);
-
-  print(a++ ==
-      --b); /* Se colocar o incremento antes do operando ele é considerado urgente, 
-  mas se está depois ele não tem precedência, neste caso processado antes da comparação '=='*/
-  print(a == b);
-
-  print(!true);
-  print(!false);
-
-  
-  // Operador Lógico Unário
-  bool x = false;
-
-  print(!x);
+  String resultado = estaChovendo && estaFrio ? 'Ficar em casa. ' : 'Sair! ';
+  print(resultado);
+  print(estaChovendo && estaFrio ? 'Azarado!' : 'Sortudo!');
 }
