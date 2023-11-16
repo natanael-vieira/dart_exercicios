1d3f463cd51eadcef1e3d37982b56d4c6f911a82
Natanael Vieira <natanaelsales@live.com>
2023-11-16 06:30:16 -0300

    Update for_1.dart

--- controle/for_1.dart
+++ controle/for_1.dart
@@ -15,5 +15,11 @@ main() {
 
   print('[FORA DO LAÇO] b = $b');
 
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+
+  for (var i = 0; i < notas.length; i++) {
+    print('Notas ${i + 1} = ${notas[i]}!');
+  }
+
   print('Fim!');
 }
