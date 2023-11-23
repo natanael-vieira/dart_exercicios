fbd3ec784f962c2530c2ab44c1df00934432461d
Natanael Vieira <natanaelsales@live.com>
2023-11-23 05:37:10 -0300

    Create classe_data_3.dart

---
+++ poo/classe_data_3.dart
@@ -0,0 +1,47 @@
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
+  String obterFormatada() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString() {
+    return obterFormatada();
+  }
+}
+
+main() {
+  var dataAniversario = new Data(9, 10, 1987);
+  // dataAniversario.dia = 9;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 1987;
+
+  Data dataCompra = Data(1, 1, 1970);
+  // dataCompra.dia = 23;
+  dataCompra.mes = 12;
+  dataCompra.ano = 2023;
+
+  String d1 = dataAniversario.obterFormatada();
+
+  print("A data do aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterFormatada()}");
+
+  print(dataCompra);
+  print(dataCompra.toString());
+
+  print(new Data());
+  print(Data(9));
+  print(Data(9, 10));
+  print(Data(9, 10, 1987));
+}
+
